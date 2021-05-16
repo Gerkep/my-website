4ddf8d017e72b32cbe0fdcaad5d054e2384071c0
Piotr Gerke <piotrg2003@gmail.com>
2021-05-16 18:27:19 +0200

    email sending fixed

--- src/main/java/com/mywebapp/personal_website/domain/MyMail.java
+++ src/main/java/com/mywebapp/personal_website/domain/MyMail.java
@@ -2,40 +2,18 @@ package com.mywebapp.personal_website.domain;
 
 public class MyMail {
 
-    private String mail;
-    private String title;
     private String text;
 
-    public MyMail(String mail, String title, String text) {
-        this.mail = mail;
+    public MyMail(String text) {
         this.text = text;
-        this.title = title;
-    }
-
-    public MyMail() {
-    };
-
-    public void setEmail(String mail) {
-        this.mail = mail;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getMail() {
-        return mail;
     }
 
     public String getText() {
         return text;
     }
 
-    public String getTitle() {
-        return title;
+    public void setText(String text) {
+        this.text = text;
     }
+
 }
--- src/main/java/com/mywebapp/personal_website/mail/EmailService.java
+++ src/main/java/com/mywebapp/personal_website/mail/EmailService.java
@@ -1,24 +1,22 @@
-package com.mywebapp.personal_website.controller;
+package com.mywebapp.personal_website.mail;
+
+import com.sendgrid.SendGrid;
+
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.mywebapp.personal_website.domain.MyMail;
 import com.sendgrid.*;
 
-@RestController
-public class EmailController {
+@Service
+public class EmailService {
 
-    @PostMapping("/send")
-    private Response sendEmail(@ModelAttribute MyMail email, Model model) {
+    public Response sendEmail(MyMail email) {
         Email from = new Email("messagerobot0@gmail.com");
         String subject = "Nowa wiadomość ze strony";
         Email to = new Email("piotrg2003@gmail.com");
-        Content content = new Content("text/plain", email.getText());
+        Content content = new Content("text/plain", "email.getText()");
         Mail mail = new Mail(from, subject, to, content);
         Response respons = null;
         SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
